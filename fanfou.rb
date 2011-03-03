2a6af70525b50c58d8ddbc6344f35fe16ec52ea5
liuw <liuw@liuw.name>
2011-03-03 23:57:02 +0800

    Refactor code, make it neater.

--- fanfou.rb
+++ fanfou.rb
@@ -2,117 +2,75 @@ require 'net/http'
 require 'rss/1.0'
 require 'rss/2.0'
 
+
 class Fanfou
 
   API_PATH = 'http://api.fanfou.com'
+  FORMATS = [:html, :rss, :xml, :json]
 
   def initialize(l, p)
     @loginname = l
     @password  = p
   end
 
-  def get_followers_ids_xml
-    response = nil
-    url = URI.parse(API_PATH)
-    Net::HTTP.start(url.host, url.port) do |http|
-      req = Net::HTTP::Get.new('/followers/ids.xml')
-      req.basic_auth @loginname, @password
-      begin
-        response = http.request(req)
-      rescue
-      end
-    end
-    response
+  def get_followers_ids(format=:json)
+    return nil if not FORMATS.include?(format)
+    get(API_PATH+'/followers/ids.'+format.to_s)
   end
 
-  def get_followers_ids_json
-    response = nil
-    url = URI.parse(API_PATH)
-    Net::HTTP.start(url.host, url.port) do |http|
-      req = Net::HTTP::Get.new('/followers/ids.json')
-      req.basic_auth @loginname, @password
-      begin
-        response = http.request(req)
-      rescue
-      end
-    end
-    response
+  def get_friends_ids(format=:json)
+    return nil if not FORMATS.include?(format)
+    get(API_PATH+'/friends/ids.'+format.to_s)
   end
 
-  def get_friends_ids_xml
-    response = nil
-    url = URI.parse(API_PATH)
-    Net::HTTP.start(url.host, url.port) do |http|
-      req = Net::HTTP::Get.new('/friends/ids.xml')
-      req.basic_auth @loginname, @password
-      begin
-        response = http.request(req)
-      rescue
-      end
-    end
-    response
-  end
-
-  def get_friends_ids_json
-    response = nil
-    url = URI.parse(API_PATH)
-    Net::HTTP.start(url.host, url.port) do |http|
-      req = Net::HTTP::Get.new('/friends/ids.json')
-      req.basic_auth @loginname, @password
-      begin
-        response = http.request(req)
-      rescue
-      end
-    end
-    response
-  end
-
-  def get_public_timeline
-    response = nil
-    url = URI.parse(API_PATH)
-    Net::HTTP.start(url.host, url.port) do |http|
-      req = Net::HTTP::Get.new('/statuses/public_timeline.rss')
-      req.basic_auth @loginname, @password
-      begin
-        response = http.request(req)
-      rescue
-      end
-    end
-    response
+  def get_public_timeline(format=:rss)
+    return nil if not FORMATS.include?(format)
+    get(API_PATH+'/statuses/public_timeline.'+format.to_s)
   end
 
   def unfollow(uid)
-    url = URI.parse(API_PATH+'/friendships/destroy.xml')
-    req = Net::HTTP::Post.new(url.path)
-    req.basic_auth @loginname, @password
-    req.set_form_data({'id' => uid})
-
-    response = Net::HTTP.new(url.host, url.port).start {|http| http.request(req)}
-    case response
-    when Net::HTTPSuccess, Net::HTTPRedirection
-      return true
-    else
-      return false
-    end
-
+    post(API_PATH+'/friendships/destroy.xml', {'id' => uid})
   end
 
   def follow(uid)
-    url = URI.parse(API_PATH+'/friendships/create.xml')
-    req = Net::HTTP::Post.new(url.path)
-    req.basic_auth @loginname, @password
-    req.set_form_data({'id' => uid})
-
-    response = Net::HTTP.new(url.host, url.port).start {|http| http.request(req)}
-    case response
-    when Net::HTTPSuccess, Net::HTTPRedirection
-      return true
-    else
-      return false
-    end
+    post(API_PATH+'/friendships/create.xml', {'id'=>uid})
   end
 
   def is_friend?(uid)
+  end
+
+  private
+  def post(path, params)
+    return false if not path
+    url = URI.parse(path)
+    req = Net::HTTP::Post.new(url.path)
+    req.basic_auth @loginname, @password
+    req.set_form_data(params)
+    response = Net::HTTP.new(url.host, url.port).start do |http|
+      begin
+        http.request(req)
+      rescue
+      end
+    end
+    case response
+    when Net::HTTPSuccess, Net::HTTPRedirection then return true
+    else return false
+    end
+  end
+
+  def get(path)
+    return nil if not path
+    response = nil
+    url = URI.parse(path)
+    req = Net::HTTP::Get.new(url.path)
+    req.basic_auth @loginname, @password
+    response = Net::HTTP.new(url.host, url.port).start do |http|
+      begin
+        http.request(req)
+      rescue
+      end
+    end
+    response
   end
 
 end
